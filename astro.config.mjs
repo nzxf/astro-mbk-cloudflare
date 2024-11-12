92f7c0f26120731b662f85ccdad4439f1db6378c
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-12 22:22:18 +0700

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare(),
 });
