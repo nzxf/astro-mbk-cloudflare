f54fc80505e723206161ac996a14e17b43a4c584
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-12 22:35:08 +0700

    Remove old netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
